77463ca97256bb7824763f73c2178b22a2639553
Hannah Massenbauer <hannah.massenbauer@gmail.com>
2024-03-14 15:16:10 +0100

    added residual, r squ

--- Unit 4.R
+++ Unit 4.R
@@ -18,7 +18,7 @@ library(gridExtra) # used to place graphs in certain order e.g. next to each oth
 ############################ Get to know data ##################################
 
 
-  setwd("C:\\Users\\hamassen\\Documents\\GitHub\\R-Tutorium")
+  setwd("C:\\Users\\hamassen\\Documents\\GitHub\\R-Tutorium\\Data")
   data <- load("participants.csv")
 
 
@@ -42,7 +42,7 @@ library(gridExtra) # used to place graphs in certain order e.g. next to each oth
 # after
   ggplot(participants, aes(x = r_skills_after, fill = program)) +
     geom_density(alpha = 0.5) +
-    labs(title = "Kernel Density Estimate of R Skills Before",
+    labs(title = "Kernel Density Estimate of R Skills After",
          x = "R Skills",
          y = "Density") +
     theme_minimal() + 
@@ -51,6 +51,18 @@ library(gridExtra) # used to place graphs in certain order e.g. next to each oth
           plot.title = element_text(hjust = 0.5))
 
 
+# skills change
+  ggplot(participants, aes(x = r_skills_change, fill = program)) +
+    geom_density(alpha = 0.5) +
+    labs(title = "Kernel Density Estimate of R Skills Change",
+         x = "R Skills",
+         y = "Density") +
+    theme_minimal() + 
+    theme(legend.position = "bottom",
+          legend.box = "horizontal",
+          plot.title = element_text(hjust = 0.5))
+  
+  
 
 ########################## Linear Regression Model ############################# 
 
@@ -59,7 +71,7 @@ library(gridExtra) # used to place graphs in certain order e.g. next to each oth
   
   # Estimate OLS
 
-  linear_model <- lm(r_skills_change ~ tutorium_attendance, 
+  linear_model <- lm(log_skill_change ~ tutorium_attendance, 
                      data = participants)
 
 # Breakdown: 
@@ -77,7 +89,7 @@ library(gridExtra) # used to place graphs in certain order e.g. next to each oth
   
   # We can add multiple explanatory variables with "+"
   
-  linear_model_2 <- lm(r_skills_after ~ tutorium_attendance + learning_hours, data = participants)
+  linear_model_2 <- lm(log_skill_change ~ tutorium_attendance + learning_hours, data = participants)
   
   summary(linear_model_2)
   
@@ -88,100 +100,154 @@ library(gridExtra) # used to place graphs in certain order e.g. next to each oth
 # Efficiency tip:   
   # Export regression results to LaTeX for Overleaf
   
-  stargazer(linear_model_2, title = "Regression Results", header = FALSE, type = "latex", out = "regression_results.tex")
+  stargazer(linear_model_2, 
+            title = "Regression Results",
+            header = FALSE, 
+            type = "latex",                         # to where do we want export
+            out = "regression_results.tex")
   
   
   # Export regression results to Word
   
-  stargazer(linear_model_2, title = "Regression Results", header = FALSE, type = "text", out = "regression_results.docx")
+  stargazer(linear_model_2, 
+            title = "Regression Results", 
+            header = FALSE, 
+            type = "text", 
+            out = "regression_results.docx")
   
   
-
-########################### Fixed Effects Model ################################ 
+  rm(linear_model, linear_model_2, data, resid2, residuals)  # clean up 
+  
+  
+  
 
   
-  # Story: 
-  # Students from different programs have different skills and experience
-  # It is hard to catch all these factors, but it is likely that students of 
-  # the same program exhibit similar factors -> it is time-invariant and group-
-  # specific. Thus, we can use program fixed effects to capture these unobserved
-  # characteristics
-
-  fe_reg_1 <- plm(r_skills_change ~ tutorium_attendance,    # plm package
-                      data = participants,             
-                      index = c("program"),            # we have to indicate the level on which we apply fixed effects
-                      model = "within")                # within groups we are considered with unobserved characteristics
+  #################  Depict linear regression graphically #################  
   
-  summary(fe_reg_1)
-
-
-# Add a second independent variable 
-
-  fe_reg_2 <- plm(r_skills_change ~ tutorium_attendance + learning_hours , 
-                  data = participants,             
-                  index = c("program"),            
-                  model = "within")                
-  
-  summary(fe_reg_2)
-
-
-############################ What is the difference? ###########################
-
-
-# Create scatter plots to illustrate 
-
-# Regular OLS model 
-  
-model1 <-  ggplot(participants, aes(x = learning_hours, y = r_skills_change)) +
+  # 1) Run model 
     
-    geom_point() +                                          # scatter plot layer
+    model <- lm(r_skills_change ~ learning_hours, data = participants)
+
+  
+  # 2)  Create a  plot
+
+    plot(r_skills_change ~ learning_hours,   # same logic as in regression
+       data = participants, 
+       main = "Scatter Plot with Residuals",
+       xlab = "Learning hours", ylab = "R skills change", pch = 16)
+  
+  # add fitted line from OLS model
+   abline(model, col = "darkblue") 
+
+  # Recap what OLS estimator does:   
+  # fitted line = average predicted outcome depending on learning hours
+  
+  # Reflects the average value for the change in R skills one can expect, 
+  # depending on the amount of learning hours
+  
+  # Interpretation: upward slope = the more you learn the better your r skills will become
+  
+   
+   
+#####################   Residuals - what are those?   ##########################
+  
+  
+  # Extract residuals from model 
+  
+  residuals <- residuals(model)   
+  
+  
+  # Where are the residuals now? 
+  arrows(
+    # Define starting points of arrows
+    participants$learning_hours,    # X-coordinates 
+         fitted(model),             # Y-coordinates 
     
-    # Add a second layer to show slope of OLS model
-    geom_smooth(method = "lm", se = FALSE) +                # linear fit, resembles beta in regression
-    labs(title = "OLS",
-         x = "Learning hours",
-         y = "R Skills Change") +
-    theme_minimal() +
-    theme(legend.position = "bottom",
-          plot.title = element_text(hjust = 0.5))
+    # Define ending points of arrows
+         participants$learning_hours,    # X-coordinates
+         participants$r_skills_change,  # Y-coordinates 
+         
+         length = 0.1,                    # Length of the arrowheads
+         col = "grey")                    # Color of the arrows
+  
+  # The distance between line and point is the residual = what remains unexplained
+  # Less distance between points and fitted line implies a better fit
+
+
+  # Why care about distribution of residuals? 
+  # 4) Gauss-Markov Assumption: Heteroskedasticity  
+  
+  # When the variance increases depending on the x variable, 
+  # then our OLS estimator beta will be inconsistent 
+
   
 
-# Apply within transformation 
   
-model2 <-  ggplot(participants, 
-                  aes(x = learning_hours, y = r_skills_change, 
-                       color = program)) +             # differentiate color by program
+  # More reasons why we care about residuals..
+  # 5)  Gauss-Markov  Assumption: Normality   
   
-               geom_point() +                           # First layer: scatter plot   
+  qqnorm(residuals)
+  qqline(residuals) 
   
-               geom_smooth(method = "lm", se = FALSE) + # Second layer: Slope per program
-  
-                  labs(title = "Fixed effect",
-                   x = "Learning hours",
-                   y = "R Skills Change") +
-                  guides(color = guide_legend(title = "Program")) +   
-                  theme_minimal() +
-                  theme(legend.position = "bottom",
-                        plot.title = element_text(hjust = 0.5))
+  # When the errors diverge a lot from the line than the standard errors in 
+  # a finite sample can become imprecise. 
+  # This is important for e.g. t-statistics, which we interpret as "significant" or not. 
   
 
+  
+################  How much can we explain with our model   #####################  
+  
+  
+  # Hello R-squared
+  
+  summary_model <- summary(model)
+  
+  # Extract R-squared value
+  r_squared <- summary_model$r.squared
+  
+  
+  predicted_values <- predict(model)
+  
+  # Add text annotation for R-squared
+  r_squared <- summary(model)$r.squared
+  text(x = max(participants$r_skills_change) * 0.8, y = max(predicted_values) * 0.9,
+       label = paste("R-squared =", round(r_squared, 2)))
+ 
 
-###############   Combines graphs next to each other  ########################## 
-
-
-# Grid.arrange allows you to place several graphs next to each other
-
-  grid.arrange(
-    arrangeGrob(model1),     # add graph 1
-    
-    arrangeGrob(model2),     # add graph 2
-    
-    ncol = 2,                # define amount of columns
-    
-    top = "Impact of Learning hours on R Skills Change"
-  )
-
-
+  
+  
+  
+# Correlation != Coefficient from OLS regression 
+  
+  cor.test(participants$r_skills_change, participants$learning_hours)
+  
+  cor.test(participants$r_skills_change, participants$program)
+  
+  participants <- participants %>% 
+    mutate(program_numeric = case_when(       
+      program == "Informatics" ~ 1,                  
+      program == "Philosophy" ~ 2,                
+      program == "Economics" ~ 3))
+  
+  cor.test(participants$r_skills_change, participants$program_numeric)
+  
+  model2 <- lm(r_skills_change ~ factor(program), data = participants)  
+  # factor() indicates that we have distinct groups
+  # and thus want slopes for each group 
+  
+  # Uh, where is the third study program? 
+  
+  
+  r_sqr_2 <- summary(model2)$r.squared
+  
+  r_sqr_2
+  
+  
+  # Add both scatter plots in one graph and show difference in R^2 
+  
+  
+  
+  
 
 ##########################  Hypothesis Test  ###################################
   
@@ -285,5 +351,118 @@ model2 <-  ggplot(participants,
     theme(legend.position = "bottom",
           plot.title = element_text(hjust = 0.5))
   
+  
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+################################################################################
+#                                 Appendix                                     #
+################################################################################
+  
+  
+  
+  
+########################### Fixed Effects Model ################################ 
+  
+  
+  # Story: 
+  # Students from different programs have different skills and experience
+  # It is hard to catch all these factors, but it is likely that students of 
+  # the same program exhibit similar factors -> it is time-invariant and group-
+  # specific. Thus, we can use program fixed effects to capture these unobserved
+  # characteristics
+  
+  fe_reg_1 <- plm(r_skills_change ~ tutorium_attendance,    # plm package
+                  data = participants,             
+                  index = c("program"),            # we have to indicate the level on which we apply fixed effects
+                  model = "within")                # within groups we are considered with unobserved characteristics
+  
+  summary(fe_reg_1)
+  
+  
+  # Add a second independent variable 
+  
+  fe_reg_2 <- plm(r_skills_change ~ tutorium_attendance + learning_hours , 
+                  data = participants,             
+                  index = c("program"),            
+                  model = "within")                
+  
+  summary(fe_reg_2)
+  
+  
+  ############################ What is the difference? ###########################
+  
+  
+  # Create scatter plots to illustrate 
+  
+  # Regular OLS model 
+  
+  model1 <-  ggplot(participants, aes(x = learning_hours, y = r_skills_change)) +
+    
+    geom_point() +                                          # scatter plot layer
+    
+    # Add a second layer to show slope of OLS model
+    geom_smooth(method = "lm", se = FALSE) +                # linear fit, resembles beta in regression
+    labs(title = "OLS",
+         x = "Learning hours",
+         y = "R Skills Change") +
+    theme_minimal() +
+    theme(legend.position = "bottom",
+          plot.title = element_text(hjust = 0.5))
+  
+  
+  # Apply within transformation 
+  
+  model2 <-  ggplot(participants, 
+                    aes(x = learning_hours, y = r_skills_change, 
+                        color = program)) +             # differentiate color by program
+    
+    geom_point() +                           # First layer: scatter plot   
+    
+    geom_smooth(method = "lm", se = FALSE) + # Second layer: Slope per program
+    
+    labs(title = "Fixed effect",
+         x = "Learning hours",
+         y = "R Skills Change") +
+    guides(color = guide_legend(title = "Program")) +   
+    theme_minimal() +
+    theme(legend.position = "bottom",
+          plot.title = element_text(hjust = 0.5))
+  
+  
+  
+  ###############   Combines graphs next to each other  ########################## 
+  
+  
+  # Grid.arrange allows you to place several graphs next to each other
+  
+  grid.arrange(
+    arrangeGrob(model1),     # add graph 1
+    
+    arrangeGrob(model2),     # add graph 2
+    
+    ncol = 2,                # define amount of columns
+    
+    top = "Impact of Learning hours on R Skills Change"
+  )
   
   
